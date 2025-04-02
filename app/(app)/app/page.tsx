7714e07992409f7361098fb2d1c6a0aba5ebb76e
Aniruddh Nagare <codebyanii@gmail.com>
2025-04-02 01:21:35 -0700

    2 april

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-6 p-4">
-      <Welcome name="Reza" />
+      <Welcome name="Sir" />
       <Search />
       <Stats />
       <Suspense fallback={<ProductsListLoading />}>
